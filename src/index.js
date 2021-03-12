a653054e70d9f747d9993d5dcb7133774a03c363
Yulia <Yulia159V@gmail.com>
2021-03-12 19:02:25 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == null || array.length == 0) {
+    return 0;
+  }
+  return Math.min.apply (null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null || array.length == 0) {
+    return 0;
+  }
+  return Math.max.apply (null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == null || array.length == 0) {
+    return 0;
+  }
+  let arr = 0;
+  for (i = 0; i < array.length; i++) {
+      arr += array[i];
+  }
+  return arr/array.length;
 }
